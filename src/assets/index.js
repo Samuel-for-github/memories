c9ff9c4a185ae7b60a45c7813fbb3f07ccb7d218
Samuel fernandes <samuelfernandes10001@gamil.com>
2024-01-21 22:37:11 +0530

    title changed, img src change

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import samuel from "./samuel.png";
+import gauresh from "./gauresh.png";
+import niosh from "./niosh.png";
+import noah from "./noah.png";
+import sumit from "./sumit.png";
+export {samuel,gauresh,niosh, noah, sumit }
